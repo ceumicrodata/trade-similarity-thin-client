e92e989cbdec40eb96e6de82d5c110f50d945777
Miklos Koren <miklos.koren@gmail.com>
2019-07-01 12:02:22 +0200

    Minimal working example

--- heatmap.py
+++ heatmap.py
@@ -1,76 +1,22 @@
 import os
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
+import json
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 from dash import Dash
 
-THETA = 8.0
+slopechart_dict = json.load(open('data/slopechart.json'))
+heatmap_dict = json.load(open('data/heatmap.json'))
+YEARS = json.load(open('data/years.json'))
+PARTNERS = json.load(open('data/partners.json'))
+NEW_MEMBER_STATES = json.load(open('data/new_member_states.json'))
 
-YEARS = ['{}'.format(y) for y in range(2001, 2018)]
-
-NEW_MEMBER_STATES = 'CZ HU PL SI SK LV LT HR BG RO'.split()
-CANDIDATE_COUNTRIES = 'AL BA TR MK ME RS'.split()
-NEIGHBORHOOD_COUNTRIES = 'DZ MA TN AM AZ GE MD UA'.split()
-PARTNERS = 'RU CN US'.split() + CANDIDATE_COUNTRIES + NEIGHBORHOOD_COUNTRIES
-
-country_codes = pd.read_csv('data/countries.csv')
-export_data = pd.read_csv('data/TC_INDEX_EXP_.csv',index_col=0)
-import_data = pd.read_csv('data/TC_INDEX_EXP_.csv',index_col=0)
-
-export_data = export_data.merge(country_codes, how='left', left_on='DECLARANT', right_on='iso3166_2').rename(columns={'country_name': 'declarant_name'})
-export_data = export_data.merge(country_codes, how='left', left_on='PARTNER', right_on='iso3166_2').rename(columns={'country_name': 'partner_name'})
-export_data = export_data[export_data.PARTNER.isin(PARTNERS)]
-
-def convert_index(KLD):
-    return pd.np.exp(-1/THETA * KLD)
-
-def sort_data(Z):
-    z = Z.values
-    Z['sort'] = z.mean(axis=1)
-    Z = Z.sort_values(by='sort', ascending=True).drop(columns='sort')
-    return Z
-
-def prepare_data(data, year):
-    Z = sort_data(
-      convert_index(
-        data.pivot(
-          index='declarant_name', 
-          columns='partner_name', 
-          values='TCI_{}'
-          .format(year)
-          )
-        )
-      )
-
-    z = Z.values
-    x = Z.columns.values
-    y = Z.index.values
-    return (x, y, z)
-
-def update_slopechart(Partner):
+def update_slopechart(_, selected):
     Declarants, Emphasized = NEW_MEMBER_STATES, []
-    data = export_data[export_data.DECLARANT.isin(Declarants)].query('PARTNER=="{}"'.format(Partner))
+    print(slopechart_dict[selected][0])
     return {
-        'data': [
-        go.Scatter(
-            x = YEARS,
-            y = [convert_index(data.query('DECLARANT=="{}"'.format(d))['TCI_{}'.format(year)].values[0]) for year in YEARS],
-            text = 'Trade between {} and {}'.format(d, Partner),
-            mode = 'lines',
-            line = {
-                'color': 'lightgrey',
-                'width': 0.5
-            },
-            marker = {
-                'size': 15,
-                'opacity': 0.5,
-                'line': {'width': 0.5, 'color': 'lightgrey'}
-            },
-            name = d
-        )
-        for d in Declarants],
+        'data': [go.Scatter(**line) for line in slopechart_dict[selected]],
         'layout': go.Layout(
             xaxis={
                 'title': "Year",
@@ -134,7 +80,7 @@ layout = html.Div([
                    style={"display": "block", "margin-left": "auto", "margin-right": "auto", "width": "60%"}),
 
         dcc.Graph(id="slopechart",
-                figure = update_slopechart('RU'),
+                figure = update_slopechart(None, 'RU'),
                 style={"margin-right": "auto", "margin-left": "auto", "width": "60%"})],
                 className="row"),
 
@@ -181,9 +127,8 @@ app.layout = layout
 #    Output("slopechart", "figure"),
 #    [Input("selected-partner", "value")])
 
-def update_figure(selected):
-    x, y, z = prepare_data(export_data, selected)
-    trace = go.Heatmap(x=x, y=y, z=z, colorscale='Electric', colorbar={"title": "KDL"}, showscale=True, zauto=False, zmin=0, zmax=1)
+def update_figure(selected, _):
+    trace = go.Heatmap(**heatmap_dict[selected], colorscale='Electric', colorbar={"title": "KDL"}, showscale=True, zauto=False, zmin=0, zmax=1)
     return {"data": [trace],
             "layout": go.Layout(width=800, height=750, title=f"{selected.title()}", 
                                 xaxis={"title": "Partner"},
