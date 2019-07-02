c3d35d7578bfcea68c0b4d3f48e758960487a053
Miklos Koren <miklos.koren@gmail.com>
2019-07-02 21:34:58 +0200

    fix #1, fix #2, fix #3

--- heatmap.py
+++ heatmap.py
@@ -33,10 +33,16 @@ layout = html.Div([
           ***
           ## Motivation
           We use detailed product-level trade data to construct a Trade Similarity Index for each Member State. This involves measuring the external export and import structure towards third countries. The index captures the relevant economic structure of the Member States (for exports) and changes in applied external tariffs (for imports). A low value of the Trade Similarity Index for a Member State suggests that it has incentives to use national trade and investment promotion instruments to offset at least to some extent the effects of adopting the common commercial policy of the EU. The Trade Similarity Index provides valuable information on changes in trade structure over time and the incentives for national agencies to pursue idiosyncratic policies.
-
-          The Trade Similarity Index takes values between 0 and 1. A higher value indicates that the trade patter on the Member State is *more similar* to the average of the European Union. A value of 0 means that there is no product overlap between the exports (or imports) of the Member State and the rest of the EU. A value of 1 means that the Member State exports (or imports) every product in the same proportion as the rest of the EU.
+          ***
+          The Trade Similarity Index takes values between 0 and 1. A higher value indicates that the trade pattern of the Member State is *more similar* to the average of the European Union. A value of 0 means that there is no product overlap between the exports (or imports) of the Member State and the rest of the EU. A value of 1 means that the Member State exports (or imports) every product in the same proportion as the rest of the EU.
 
           ***
+          # Cross Sectional Patterns
+
+          The following chart displays the Trade Similarity Index for all 28 Member States. The index can be computed vis-a-vis any third country with which both the Member State and the rest of the EU trade. In the heatmap we display it for selected third countries.
+          ***
+          A low value of the index between a Member State and a third country means that that Member State has special trade relations with that country, with low similarity to other EU countries. For example, the exports of Malta to Montenegro in 2017 is the least similar to EU average, with a Trade Similarity Index of only 0.20. By contrast, German exports to Russia in 2017 are the most similar to exports of other EU countries to Russia (TSI=0.93). Looking for cases of dissimilarity can reveal country pairs where the trade patterns are least similar to the EU average, and the incentive to deviate from a common trade policy is the largest.
+
           ''', 
           className='container',
           style = {'maxWidth': '650px'}
@@ -53,23 +59,13 @@ layout = html.Div([
         dcc.Graph(id="heatmap", style={"margin-right": "auto", "margin-left": "auto", "width": "60%"}, className='row'),
 
         dcc.Markdown('''
-          ***
-          ## Data and Methodology
-          ### Data
-          1. Data is downloaded from Eurostat Comext at [URL]
-          2. We only keep trade flows in normal statistical regime (excluding, for example, processing trade)
-          3. We aggregate all trade flows up to 6-digit Harmonized System products.
-          4. There are cases when product codes are masked to protect confidentiality of individual sellers, affecting about 2--3 percent of trade value. Because these product codes are available at the Chapter (2-digit) level, we redistribute the total value of confidential trade across the reported, non-confidential product codes of the same Chapter.
+          # Trends
 
-          ### Computation
-          $$ \\sum_\{p=1\}^P s_{ijp}\\ln(s_{ijp}/s_{jp}) $$
-          This is the [Kullback-Leibler divergence](https://en.wikipedia.org/wiki/Kullback%E2%80%93Leibler_divergence) between the product shares (i,j) and the product shares (baseline,j).
+          This chart shows the evolution of the Trade Similarity Index of new Member States vis-a-vis select third countries. Broadly speaking, the export composition of new Member States becomes more and more similar to the rest of the EU over time.
           ''', 
           className='container',
           style = {'maxWidth': '650px'}
         ),
-
-        html.P('$$ \\sum_\{p=1\}^P s_{ijp}\\ln(s_{ijp}/s_{jp}) $$'),
 
         dcc.Dropdown(id="selected_partner", options=[{"label": country_codes[i], "value": i} for i in PARTNERS],
                    value='RU',
@@ -80,30 +76,80 @@ layout = html.Div([
                 className="row"),
 
         dcc.Markdown('''
+          ## Data and Methodology
+          ### Data
+          1. Data is downloaded from Eurostat Comext (Eurostat, 2019).
+          2. We only keep trade flows in normal statistical regime (excluding, for example, processing trade).
+          3. We aggregate all trade flows up to 6-digit Harmonized System products.
+          4. There are cases when product codes are masked to protect confidentiality of individual sellers, affecting about 2--3 percent of trade value. Because these product codes are available at the Chapter (2-digit) level, we redistribute the total value of confidential trade across the reported, non-confidential product codes of the same Chapter.
+
+          ### Computation
+          We compare the export- and import-composition of a Member State vis-a-vis a third country with the EU average. Let *s(ijp)* denote the export share of product *p* in total exports of country *i* to country *j*. We compute a baseline export share *s(0jp)* as the export share of all other countries in the EU. We are interested in how similar is *s(ijp)* to *s(0jp)*.
+          ***
+          We first compute the Kullback-Leibler divergence (Kullback, 1987) between the two trade share vectors as
+          ''', 
+          className='container',
+          style = {'maxWidth': '650px'}
+        ),
+        html.Div(html.Img(src=app.get_asset_url('KLD.png')),
+          className='container',
+          style = {'maxWidth': '650px', 'textAlign': 'center'}
+        ),
+        dcc.Markdown('''
+          As the name suggests, the Kullback-Leibler divergence is a measure of distance.
+          This measure takes a value of 0 if the two vectors are the same, and can be arbitrarily large for different vectors.
+
+          We convert it into a Trade Similarity Index as  
+          ''', 
+          className='container',
+          style = {'maxWidth': '650px'}
+        ),
+        html.Div(html.Img(src=app.get_asset_url('utility.png')),
+          className='container',
+          style = {'maxWidth': '650px', 'textAlign': 'center'}
+        ),
+        dcc.Markdown('''
+          where theta is a positive parameter, capturing how substitutable the different products are in the eyes of the consumer.
+
+          This is an index of *similarity*, taking larger values whenever the two vectors are more similar. The index is bounded between 0 and 1, being 0 if the two vectors have no common elements, and 1 if the two vectors are identical. 
+
+          The theoretical justification of this index (as noted on the left-hand-side of the equation) is that consumers evaluating choices across products with an elasticity of substitution theta would value a forced substitution this much less. That is, if a consumer would prefer to consumer *s(ijp)* but is forced to consume *s(0jp)* instead, her utility is lower by a factor corresponding to the Trade Similarity Index. Hence an index of 0.75 means a utility loss of 25 percent.   
+
           ## State of the art
 
-          All indexes of similarity start with a vector of trade shares si and sj and compute
-          $$
-          sum_p f(s_\{ip}, s_\{jp}),
-          $$  
-          for various functions $f$.
+          All indexes of similarity start with a vector of sector/product shares *si* and *sj* and compute
+          ''', 
+          className='container',
+          style = {'maxWidth': '650px'}
+        ),
 
-          The Krugman Specialization Index (Krugman, 1991) uses \\(f(x, y) = |x - y|\\). This index captures the absolute percentage deviation between trade shares.
+        html.Div(html.Img(src=app.get_asset_url('index.png')),
+          className='container',
+          style = {'maxWidth': '650px', 'textAlign': 'center'}
+        ),
 
-          An alternative measure is the Finger-Kreining index (Finger and Kreinin, 1979), with $f(x) = min(x, y)$, capturing the least amount of overlap between the two trade shares.
+        dcc.Markdown('''
+          for various functions *f*.
 
-          Fontagné et al (2018) use dissimularity measures for binary vectors, with $s_\{ip\}in \{0,1\}$, such as the Levenshtein distance and the Bray-Curtis measure.
+          The Krugman Specialization Index (Krugman, 1991) uses *f*(*x*, *y*) = |*x* - *y*|. This index captures the absolute percentage deviation between trade shares.
 
-          None of these indexes are based on economic theory. By contract assume that consumers have CES preferences over the individual products, with elasticity of substitution sigma. 
-          ```
-          f(x, y) = x^{1/sigma} y^{1-1/sigma}
-          ```
-          In the limite, when $sigma to 1$, this index converges to the Kullback-Leibler divergence.
+          An alternative measure is the Finger-Kreining index (Finger and Kreinin, 1979), with *f*(*x*) = min(*x*, *y*), capturing the least amount of overlap between the two trade shares.
 
-          ### References
-          - Krugman, Paul. 1991. Geography and Trade. Cambridge: MIT Press.
+          Fontagné et al (2018) use dissimularity measures for binary vectors, with *sip* in \{0,1\}, such as the Levenshtein distance and the Bray-Curtis measure.
+
+          In contrast to our measure of similarity, none of these indexes are based on economic theory.
+          ''', 
+          className='container',
+          style = {'maxWidth': '650px'}
+        ),
+
+         dcc.Markdown('''
+         ### References
+          - Eurostat. 2019. “COMEXT: International Trade in Goods.” http://epp.eurostat.ec.europa.eu/newxtweb/mainxtnet.do.
           - Finger, J. M., and M. E. Kreinin. 1979. “A Measure of 'Export Similarity' and Its Possible Uses.” The Economic Journal 89 (356): 905–12.
           - Fontagné, Lionel, Angelo Secchi, and Chiara Tomasi. 2018. “Exporters’ Product Vectors across Markets.” European Economic Review 110 (November): 150–80.
+          - Krugman, Paul. 1991. Geography and Trade. Cambridge: MIT Press.
+          - Kullback, Solomon. 1987. “Letters to the Editor: The Kullback-Leibler Distance.” The American Statistician 41 (4): 338–41.
           ''', 
           className='container',
           style = {'maxWidth': '650px'}
